7ca028b27b8a8cf78688ffa3348fb4ba55c01452
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-12-16 18:55:46 +0530

    ghg

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ const port = 8080
 
 const onePageArticleCount = 10
 
+const { newsArticleModel } = require('./connector')
+const { data } = require('./data')
+
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+const middleWare = (req,res,next)=>{
+    let {limit , offset} = req.query ; 
+    req.query.limit = Number(limit)?Number(limit):10;
+    req.query.offset = Number(offset)?Number(offset):0;
+    console.log(req.query);
+    next();
+}
+
+app.get("/newFeeds",middleWare, async (req,res)=>{
+    let {limit , offset} = req.query ;
+    res.send(await newsArticleModel.find().skip(offset).limit(limit));
+})
 
 
 
